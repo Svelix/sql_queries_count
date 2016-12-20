ca08a72a24b2ea32945302911b2c3526d64b9c48
GregPK <grzegorz.kaczorek@gmail.com>
2016-12-20 18:00:13 +0100

    Ignore certain query types, distinguish between cached and non-cached queries

--- lib/sql_queries_count/controller_runtime.rb
+++ lib/sql_queries_count/controller_runtime.rb
@@ -7,25 +7,28 @@ module SqlQueriesCount
 
     protected
 
-      attr_internal :db_runtime
+    attr_internal :db_runtime
 
-      def process_action(action, *args)
-        QueryCounter.reset_counter
-        super
-      end
+    def process_action(action, *args)
+      QueryCounter.reset_counter
+      QueryCounter.reset_cached_counter
+      super
+    end
 
-      def append_info_to_payload(payload)
-        super
-        if ActiveRecord::Base.connected?
-          payload[:db_query_count] = QueryCounter.reset_counter
-        end
+    def append_info_to_payload(payload)
+      super
+      if ActiveRecord::Base.connected?
+        payload[:db_query_count] = QueryCounter.reset_counter
+        payload[:db_cached_query_count] = QueryCounter.reset_cached_counter
       end
+    end
 
 
     module ClassMethods
       def log_process_action(payload)
-        messages, sql_count = super, payload[:db_query_count]
+        messages, sql_count, sql_cached_count = super, payload[:db_query_count], payload[:db_cached_query_count]
         messages << ("SQL count: %d" % sql_count) if sql_count
+        messages << ("SQL count (cached): %d" % sql_cached_count) if sql_cached_count
         messages
       end
     end
--- lib/sql_queries_count/query_counter.rb
+++ lib/sql_queries_count/query_counter.rb
@@ -9,9 +9,22 @@ module SqlQueriesCount
       Thread.current["active_record_sql_count"] ||= 0
     end
 
+    def self.cached_counter=(value)
+      Thread.current["active_record_cached_sql_count"] = value
+    end
+
+    def self.cached_counter
+      Thread.current["active_record_cached_sql_count"] ||= 0
+    end
+
     def self.reset_counter
-     rt, self.counter = counter, 0
-     rt
+      rt, self.counter = counter, 0
+      rt
+    end
+
+    def self.reset_cached_counter
+      rt, self.cached_counter = cached_counter, 0
+      rt
     end
 
     def initialize
@@ -19,10 +32,15 @@ module SqlQueriesCount
     end
 
     def sql(event)
-      self.class.counter += 1
+      ignore = [/^PRAGMA (?!(table_info))/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /^SAVEPOINT/, /^ROLLBACK TO SAVEPOINT/, /^RELEASE SAVEPOINT/, /^SHOW max_identifier_length/, /^SET/]
+      return if ignore.any? { |r| event.payload[:sql] =~ r }
+
+      if event.payload[:name] == 'CACHE'
+        self.class.cached_counter += 1
+      else
+        self.class.counter += 1
+      end
     end
   end
 
 end
-
-
